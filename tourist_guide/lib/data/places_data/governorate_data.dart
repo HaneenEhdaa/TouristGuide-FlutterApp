d2398906f9efc71d827b3693e2835eb9e816f2d3
mohammedtohamym <mohammedtohamym@gmail.com>
2025-01-10 13:55:42 +0200

    add governorate data structure

---
+++ tourist_guide/lib/data/places_data/governorate_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:tourist_guide/data/models/governorate_model.dart';
+
+class GovernorateData {
+  static List<Governorate> governorateList = [
+    Governorate(
+        id: 'id',
+        imgPath: [Image.asset('assets/images/sphinx2.jpg')],
+        name: 'name'
+    )
+  ];
+}
