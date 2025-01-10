7d3ce491dcb33f26d3ce245af8cbba249e569a92
HaneenEhdaa <116807965+HaneenEhdaa@users.noreply.github.com>
2025-01-10 02:12:10 +0200

    Add Favorite Screen

--- tourist_guide/lib/ui/landmarks/fav_screen.dart
+++ tourist_guide/lib/ui/landmarks/fav_screen.dart
@@ -1,12 +1,80 @@
+import 'package:tourist_guide/core/widgets/fav_card.dart';
+import 'package:tourist_guide/data/models/landmark_model.dart';
+import 'package:tourist_guide/data/places_data/places_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({super.key});
 
   @override
+  State<FavoritesScreen> createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  List<LandMark> Fav_list = PlacesData().favoritePlaces();
+
   Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Favorite Places"),
+              backgroundColor: Colors.white,
+            ),
+            body: PlacesData().favoritePlaces().isEmpty
+                ? _noFav()
+                : SizedBox(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Flexible(
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: PlacesData().favoritePlaces().length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: EdgeInsets.only(
+                                    left: index == 0 ? 10.w : 0, right: 10.w),
+                                child: FavCard(
+                                  place: Fav_list[index],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+            backgroundColor: Colors.white,
+          );
+        });
+  }
+
+  Widget _noFav() {
     return Center(
-      child: const Text('Favorites Screen'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.favorite_outline,
+            color: Color.fromARGB(170, 222, 114, 84),
+            size: 150,
+          ),
+          Text(
+            "Add Your Favorite Places",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+              fontSize: 18.sp,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
