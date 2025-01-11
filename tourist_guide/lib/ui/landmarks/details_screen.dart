40c9aca991b4bc6840d74b855eaf1161549b24a0
Mostafa Saad <83826642+Mostafa11412@users.noreply.github.com>
2025-01-11 18:43:01 +0200

    commenting code final time.

--- tourist_guide/lib/ui/landmarks/details_screen.dart
+++ tourist_guide/lib/ui/landmarks/details_screen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: must_be_immutable
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,28 +10,28 @@ import 'package:tourist_guide/data/models/landmark_model.dart';
 import 'package:tourist_guide/data/places_data/places_data.dart';
 
 class DetailsScreen extends StatefulWidget {
-  DetailsScreen({
-    super.key,
-  });
+  DetailsScreen({super.key});
 
   @override
   State<DetailsScreen> createState() => _DetailsScreenState();
 }
 
 class _DetailsScreenState extends State<DetailsScreen>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final List<Animation<double>> _fadeAnimations;
+    with SingleTickerProviderStateMixin { //using singleTricler because using  one animation controller.
+  late final AnimationController _controller; // Controller for managing animations.
+  late final List<Animation<double>> _fadeAnimations; // Fade animations for various elements.
 
   @override
   void initState() {
     super.initState();
 
+    // Initialize the animation controller with a duration of 5 seconds.
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 5),
     );
 
+    // Create staggered fade animations..
     _fadeAnimations = List.generate(
       4,
       (index) => Tween<double>(begin: 0.0, end: 1.0).animate(
@@ -45,12 +46,13 @@ class _DetailsScreenState extends State<DetailsScreen>
       ),
     );
 
+    // Start the animations.
     _controller.forward();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller.dispose(); // Dispose of the controller to free resources.
     super.dispose();
   }
 
@@ -61,27 +63,22 @@ class _DetailsScreenState extends State<DetailsScreen>
 
     return Scaffold(
       body: Padding(
-        padding: REdgeInsets.symmetric(
-          horizontal: 15,
-        ),
+        padding: REdgeInsets.symmetric(horizontal: 15),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                height: 50.h,
-              ),
+              SizedBox(height: 50.h), 
               FadeTransition(
-                  opacity: _fadeAnimations[0],
-                  child: _coverImage(context, landMark)),
-              SizedBox(
-                height: 16.h,
+                opacity: _fadeAnimations[0],
+                child: _coverImage(context, landMark), // Cover image widget
               ),
+              SizedBox(height: 16.h),
               FadeTransition(
-                  opacity: _fadeAnimations[1], child: _placeDetails(landMark)),
-              SizedBox(
-                height: 16.h,
+                opacity: _fadeAnimations[1],
+                child: _placeDetails(landMark), // Landmark details section
               ),
+              SizedBox(height: 16.h),
               FadeTransition(
                 opacity: _fadeAnimations[2],
                 child: Text(
@@ -92,10 +89,12 @@ class _DetailsScreenState extends State<DetailsScreen>
                       TextStyle(fontWeight: FontWeight.w600, fontSize: 18.sp),
                 ),
               ),
+              // Check if there are nearby places and display them if available.
               PlacesData().nearbyPlaces(landMark).isNotEmpty
                   ? FadeTransition(
                       opacity: _fadeAnimations[3],
-                      child: _similiarPlaces(landMark))
+                      child: _similiarPlaces(landMark),
+                    )
                   : SizedBox(),
             ],
           ),
@@ -104,6 +103,7 @@ class _DetailsScreenState extends State<DetailsScreen>
     );
   }
 
+  // Widget to display landmark details like name, location, and rating.
   Widget _placeDetails(LandMark landMark) {
     return Column(
       children: [
@@ -117,10 +117,7 @@ class _DetailsScreenState extends State<DetailsScreen>
         ),
         Row(
           children: [
-            Icon(
-              Icons.place,
-              color: kMainColor,
-            ),
+            Icon(Icons.place, color: kMainColor),
             Text(
               landMark.governorate,
               style: TextStyle(
@@ -129,11 +126,7 @@ class _DetailsScreenState extends State<DetailsScreen>
                   color: kMainColor),
             ),
             Spacer(),
-            Icon(
-              Icons.star,
-              color: kMainColor,
-              size: 30,
-            ),
+            Icon(Icons.star, color: kMainColor, size: 30),
             Text(
               landMark.rate,
               style: TextStyle(
@@ -147,39 +140,45 @@ class _DetailsScreenState extends State<DetailsScreen>
     );
   }
 
+  // Widget to display the cover image with a back button and favorite button.
   Widget _coverImage(BuildContext context, LandMark landMark) {
     return Stack(
       children: [
         SizedBox(
           height: 400.h,
           child: CarouselSlider.builder(
-              itemCount: landMark.imgPath.length,
-              itemBuilder: (context, index, realIndex) => ClipRRect(
-                    borderRadius: BorderRadius.circular(34),
-                    child: Image(
-                      image: landMark.imgPath[index].image,
-                      fit: BoxFit.cover,
-                      height: 400.h,
-                      width: double.infinity,
-                    ),
-                  ),
-              options: CarouselOptions(
-                  autoPlay: true,
-                  autoPlayInterval: const Duration(seconds: 3),
-                  viewportFraction: 1,
-                  height: 400.h)),
+            itemCount: landMark.imgPath.length,
+            itemBuilder: (context, index, realIndex) => ClipRRect(
+              borderRadius: BorderRadius.circular(34),
+              child: Image(
+                image: landMark.imgPath[index].image,
+                fit: BoxFit.cover,
+                height: 400.h,
+                width: double.infinity,
+              ),
+            ),
+            options: CarouselOptions(
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              viewportFraction: 1,
+              height: 400.h,
+            ),
+          ),
         ),
-        _backButton(context),
+        _backButton(context), // Back button to navigate to the previous screen.
         Positioned(
           right: 15,
           bottom: 10,
-          child:
-              FavoriteButton(place: landMark, refresh: () => setState(() {})),
+          child: FavoriteButton(
+            place: landMark,
+            refresh: () => setState(() {}),
+          ),
         ),
       ],
     );
   }
 
+  // Widget for a back button.
   Widget _backButton(BuildContext context) {
     return GestureDetector(
       onTap: () => Navigator.of(context).pop(),
@@ -191,21 +190,18 @@ class _DetailsScreenState extends State<DetailsScreen>
     );
   }
 
+  // Widget to display similar places in a horizontal list.
   Widget _similiarPlaces(LandMark landmark) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Divider(
-          height: 30.h,
-        ),
+        Divider(height: 30.h),
         Text(
           'Nearby Places',
           style: TextStyle(
               fontSize: 20.sp, fontWeight: FontWeight.bold, color: kMainColor),
         ),
-        SizedBox(
-          height: 5,
-        ),
+        SizedBox(height: 5),
         SizedBox(
           height: 250.h,
           child: ListView.builder(
@@ -221,9 +217,7 @@ class _DetailsScreenState extends State<DetailsScreen>
             },
           ),
         ),
-        SizedBox(
-          height: 20.h,
-        ),
+        SizedBox(height: 20.h),
       ],
     );
   }
--- tourist_guide/lib/ui/landmarks/fav_screen.dart
+++ tourist_guide/lib/ui/landmarks/fav_screen.dart
@@ -52,7 +52,7 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
                           );
                         },
                       ),
-                    ),
+                    ), 
                   ],
                 ),
               ),
